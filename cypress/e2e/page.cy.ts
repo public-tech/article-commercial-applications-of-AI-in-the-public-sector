6a2a5cbec2f9afda93b32b15f0cf7d248c1ba4ef
Steven <steven@dominion79.co.uk>
2023-03-27 14:21:26 +0100

    Small change

--- cypress/e2e/page.cy.ts
+++ cypress/e2e/page.cy.ts
@@ -2,6 +2,6 @@
 describe('Navigation', () => {
   it('should navigate to the home page', () => {
     cy.visit('http://localhost:3000/')
-    cy.get('h1').contains('Hello world!')
+    cy.get('h1').contains('Hello world')
   })
 })
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="container mx-auto px-4">
         <h1 className="text-3xl font-bold underline">
-          Hello world!
+          Hello world
         </h1>      
     </div>    
   )
